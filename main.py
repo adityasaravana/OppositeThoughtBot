aa0e467a4bb0d02aca7d54e40da9fdf6a6b6589d
Aditya Saravana <aditya.saravana@icloud.com>
2023-12-25 16:23:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,31 +8,30 @@ from dotenv import load_dotenv
 # Load .env variables.
 load_dotenv()
 
-# Authenticate with Twitter.
-auth = tweepy.OAuthHandler(os.environ.get("TWITTER_CONSUMER_KEY"), os.environ.get("TWITTER_CONSUMER_SECRET"))
-auth.set_access_token(os.environ.get("TWITTER_ACCESS_TOKEN"), os.environ.get("TWITTER_ACCESS_TOKEN_SECRET"))
-
-# Create Tweepy API object
-tweepy_api = tweepy.API(auth)
+# Create Tweepy API object.
+tweepy_api = tweepy.Client(
+    bearer_token=os.environ.get("TWITTER_BEARER_TOKEN"),
+    access_token=os.environ.get("TWITTER_ACCESS_TOKEN"),
+    access_token_secret=os.environ.get("TWITTER_ACCESS_TOKEN_SECRET"),
+    consumer_key=os.environ.get("TWITTER_CONSUMER_KEY"),
+    consumer_secret=os.environ.get("TWITTER_CONSUMER_SECRET")
+)
 
 # Create a tweet
-tweepy_api.update_status("Hello Tweepy")
+# tweepy_api.create_tweet(text="Hello, world!")
 
 # Initialize OpenAI.
-client = OpenAI(
+openai_api = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
 # Function to convert strings to booleans.
-def str_to_bool(str):
-  if str == "True":
-    return True
-  else:
-     return False
+
+  
 
 # Function to flip tweets.
 def flip_tweet(tweet):
-    chat_completion = client.chat.completions.create(
+    chat_completion = openai_api.chat.completions.create(
         messages=[
             {
                 "role": "system",
@@ -50,22 +49,21 @@ def flip_tweet(tweet):
 
 # Function to make sure the bot doesn't accidentally say something horrible.
 def check_tweet(tweet):
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "system",
-                "content": "You make sure that a Twitter bot that tweets the opposite of what a Twitter thought leader influencer says and is therefore just as insightful doesn't say anything really bad, like bomb palestine or kill all jews. The point of the bot is to be funny and vaguely sarcastic. Response with True if its a really bad tweet, and False to go ahead and post. Make sure your response works as a Python bool, cause it's gonna get passed into code.",
-            },
-            {
-                "role": "user",
-                "content": f'Tweet: {tweet}',
-            }
-        ],
-        model="gpt-3.5-turbo-1106",
+    response = openai_api.moderations.create(
+        input=tweet
     )
-    response = chat_completion.choices[0].message.content
-    return str_to_bool(response)
+    
+    for moderation in response.results:
+        categories = moderation.categories
+        # Check if any category other than harassment is true
+        if (categories.harassment_threatening or categories.hate or 
+            categories.hate_threatening or categories.self_harm or 
+            categories.self_harm_instructions or categories.self_harm_intent or 
+            categories.sexual or categories.sexual_minors or 
+            categories.violence or categories.violence_graphic):
+            return True
+    return False
 
 flipped_tweet = flip_tweet("")
 print(flipped_tweet)
-print(check_tweet("Kill all Americans. Those fat idiots need to die."))
+print(check_tweet(""))
